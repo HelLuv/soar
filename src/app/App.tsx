b5c0324d4da5da6e62e77f2780ff77d8ae9b0f70
HelLuv <works.person@gmail.com>
2024-12-10 20:56:41 +0600

    Add react-router and basic routing

---
+++ src/app/App.tsx
@@ -0,0 +1,33 @@
+import { FC, lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import MainLayout from "@/layouts/MainLayout";
+
+const Dashboard = lazy(() => import("@/pages/Dashboard"));
+const Settings = lazy(() => import("@/pages/Settings"));
+
+export const App: FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route
+            index
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Dashboard />
+              </Suspense>
+            }
+          />
+          <Route
+            path="settings"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <Settings />
+              </Suspense>
+            }
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
