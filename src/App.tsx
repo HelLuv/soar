f43cd3057d66edbfb726b0c1c194185fc9b8f0f6
HelLuv <works.person@gmail.com>
2024-12-10 20:22:13 +0600

    Add vite-plugin-svgr

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import { FC } from "react";
 
+import HomeIcon from "./assets/icons/solid/home.svg?react";
+
 export const App: FC = () => {
-  return <main className="bg-black text-white">App</main>;
+  return (
+    <main className="bg-black text-white">
+      App
+      <HomeIcon />
+    </main>
+  );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import * as path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,13 @@ export default defineConfig({
     react(),
     checker({
       typescript: true,
-    })],
+    }),
+    svgr({
+      svgrOptions: {
+        titleProp: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
