81c386d41566d42fd90dc4dbae8ae0f59ff1a137
HelLuv <works.person@gmail.com>
2024-12-15 05:15:09 +0600

    Make BalanceHistory and ExpenseStatistics components responsive for smaller screens

--- src/modules/dashboard/BalanceHistory.tsx
+++ src/modules/dashboard/BalanceHistory.tsx
@@ -9,8 +9,8 @@ type Props = {
 
 export const BalanceHistory: FC<Props> = ({ data }) => {
   return (
-    <Section title="Balance History">
-      <div className="p-6 bg-white rounded-[25px] w-[635px] h-[270px]">
+    <Section title="Balance History" className="max-md:w-full">
+      <div className="p-6 bg-white rounded-[25px] w-[635px] h-[270px] max-md:w-full">
         <AreaChart data={data} dataKey="date" />
       </div>
     </Section>
--- src/modules/dashboard/ExpenseStatistics.tsx
+++ src/modules/dashboard/ExpenseStatistics.tsx
@@ -10,7 +10,7 @@ type Props = {
 export const ExpenseStatistics: FC<Props> = ({ data = [] }) => {
   return (
     <Section title="Expense Statistics" className="max-md:w-full">
-      <div className=" bg-white p-6 rounded-[25px] w-[350px] h-[370px] max-md:w-full">
+      <div className="bg-white p-6 rounded-[25px] w-[350px] h-[370px] max-md:w-full">
         <PieChart data={data} valuePostfix="%" />
       </div>
     </Section>
